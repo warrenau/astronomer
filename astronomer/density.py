854f341d11b473de3eec43748a5094b41877d3cd
warrenau <warrenau@oregonstate.edu>
2022-01-25 11:46:17 -0800

    added file paths to density script

--- astronomer/density.py
+++ astronomer/density.py
@@ -1,2 +1,7 @@
 # this is a script/function to process the density data
 # will need inputs of file paths to use -- this should be based on constelation
+
+# need to read in data from csv files located in \LineProbe; \LineProbeBot; and \LineProbeTop
+# axial data is denoted as: Line_Probe_Density_Line_Dens_00100.csv
+# radial data is denoted as: Radial_Line_Density_RadialDens_00100.csv
+# where _00100 at end of file name is time step, increments by 100 in folders
