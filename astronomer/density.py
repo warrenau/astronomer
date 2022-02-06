936157bfaf3ce1f49e350c9b5df531dbc0fb8a48
warrenau <warrenau@oregonstate.edu>
2022-02-05 18:00:26 -0800

    added data structure class
    added class def for density data
    added plotting section
    added new data files in Data directory
    restructured density.py file

--- astronomer/density.py
+++ astronomer/density.py
@@ -5,37 +5,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-# need to read in data from csv files located in \LineProbe; \LineProbeBot; and \LineProbeTop
-# axial data is denoted as: Line_Probe_Density_Line_Dens_00100.csv
-# radial data is denoted as: Radial_Line_Density_RadialDens_00100.csv
-# where _00100 at end of file name is time step, increments by 100 in folders
-# plot density vs time with a line for each data position on the physical experiment (I will have to find the correct positions)
-# plotting all of the time steps might not be reasonable if there are a lot -- this script or the main program should be somewhat adaptive to make good looking plots
-#folder = Path("astronomer/LineProbe/")
+
+# define class
+class Density(object):
+    """Data over time for one or more positions.
+
+    Attributes
+    ----------
+    t : time in units of [s]
+    d : density in units of [kg/m^3]
+    p : the name of the locations
+    """ # I want a way of including the header / position information, but Im not sure how rn
+
+    def __init__(self, time, data, positions):
+        """Initializes the density data with supplied values for time, density data, and location names."""
+        self.t = time
+        self.d = data
+        self.p = positions
+
+# dont use line probe data -- use the monitor csv files that have data for each position
 
 
-# read in data one file at a time -- how?
-# some sort of for loop? for file in folder?
-# set upper time step limit manually? -- can this be done automatically
-min_step = 100
-max_step = 10300
-num_steps = int(max_step/min_step)
+# read in data
+filepath = 'astronomer/Data/'
+filename = 'BOT2HENRIDensity'
+f = filepath+filename+'.csv'
+
+data_read = np.genfromtxt(f, delimiter=',')
+# need to define class or something to handle data
+time = data_read[1:,0]
+data_in = data_read[1:,1:]
+positions = ('TS00', 'TS01', 'TS02', 'TS03', 'TS04', 'TS05')
+
+data = Density(time, data_in, positions)
+
+# plot the data
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+plt.figure(facecolor='w', edgecolor='k', dpi=200)
+for k in range(len(data.p)):
+    plt.plot(data.t, data.d[:,k], label=data.p[k])
+plt.xlabel('Time (s)')
+plt.ylabel('Density (kg/m^3)')
+plt.figlegend(loc='upper left', bbox_to_anchor=(0.2,0.8))
+plt.grid(b=True, which='major', axis='both')
+plt.savefig(filepath+filename+'_plot.png',transparent=True)
+plt.savefig(filepath+filename+'_plot.svg',transparent=True)
+
+# function to convert units to atom/b-cm for MCNP
 
 
-for j in range(num_steps):
-    # generate file name
-    filename = "astronomer/LineProbe/Line_Probe_Density_LineDens_"+str('%05.0f' %(100*(j+1)))+".csv"
-    #f = folder / filename
-    # read in file
-    dens_step = np.genfromtxt(filename, delimiter=',',skip_header=1)
-
-    # set up array of zeros based on size of read in files
-    if j == 0:
-        x_num = len(dens_step[:,0]) # find length of x values
-        dens = np.zeros((x_num+1, num_steps + 1)) # set array of zeros
-        dens[:,0] = dens_step[:,0] # fill in x values
-        dens[0,1:] = np.linspace(min_step, max_step, num_steps)
-
-    # fill in with values from file read in this loop
-    dens[1:,j+1] = dens_step[:,1]
-    print(dens)
+# store the data as hdf5?
