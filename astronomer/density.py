deb6f4dc1e85cc0d351212265927abf3d56a3334
warrenau <warrenau@oregonstate.edu>
2022-02-08 11:57:19 -0800

    added density conversion function
    added a function to conver from density to atom density
    also made the plotting section a function -- may need to modify and split into smaller functions

--- astronomer/density.py
+++ astronomer/density.py
@@ -31,28 +31,83 @@ filepath = 'astronomer/Data/'
 filename = 'BOT2HENRIDensity'
 f = filepath+filename+'.csv'
 
-data_read = np.genfromtxt(f, delimiter=',')
-# need to define class or something to handle data
-time = data_read[1:,0]
-data_in = data_read[1:,1:]
 positions = ('TS00', 'TS01', 'TS02', 'TS03', 'TS04', 'TS05')
 
-data = Density(time, data_in, positions)
+def plot_data(filename, positions):
+    """ Plots data from csv file into png and svg formats.
+    Parameters
+    ----------
+    filename : string
+        name of file to be plotted
+    positions : string
+        names of positions or locations for data, like a header.
 
-# plot the data
-plt.rcParams['font.family'] = 'serif'
-plt.rcParams['mathtext.fontset'] = 'dejavuserif'
-plt.figure(facecolor='w', edgecolor='k', dpi=200)
-for k in range(len(data.p)):
-    plt.plot(data.t, data.d[:,k], label=data.p[k])
-plt.xlabel('Time (s)')
-plt.ylabel('Density (kg/m^3)')
-plt.figlegend(loc='upper left', bbox_to_anchor=(0.2,0.8))
-plt.grid(b=True, which='major', axis='both')
-plt.savefig(filepath+filename+'_plot.png',transparent=True)
-plt.savefig(filepath+filename+'_plot.svg',transparent=True)
+    Returns
+    -------
+    data : Density
+        Formatted data from csv file. # maybe this should be a separate function.
+    """
+
+    filepath = 'astronomer/Data/'
+    f = filepath+filename+'.csv'
+
+    data_read = np.genfromtxt(f, delimiter=',')
+    # need to define class or something to handle data
+    time = data_read[1:,0]
+    data_in = data_read[1:,1:]
+
+
+    data = Density(time, data_in, positions)
+
+    # plot the data
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+    plt.figure(facecolor='w', edgecolor='k', dpi=200)
+    for k in range(len(data.p)):
+        plt.plot(data.t, data.d[:,k], label=data.p[k])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Density (kg/m^3)')
+    plt.figlegend(loc='upper left', bbox_to_anchor=(0.2,0.8))
+    plt.grid(b=True, which='major', axis='both')
+    plt.savefig(filepath+filename+'_plot.png',transparent=True)
+    plt.savefig(filepath+filename+'_plot.svg',transparent=True)
+
+    return data
 
 # function to convert units to atom/b-cm for MCNP
+def density_to_atomdensity(data_in):
+    """ Converts density in kg/m^3 to atom/barn-cm for helium-3.
+    Parameters
+    ----------
+    data_in : Density
+        Density over time in kg/m^3 in Density class format.
+
+    Returns
+    -------
+    data_out : Density
+        Density over time in atom/b-cm in Density class format.
+    """
+    data_out = Density(data_in.t, data_in.d, data_in.p)
+    data_out.d = data_out * 1000 * 6.022e23 * 1e-6 * 1e-24 / 3.016029
+    return data_out
+
+
+
+# function to get time step data for MCNP input
+def get_time_step_data(data_in):
+    """ Sparses data file for time steps approx every 1 millisecond.
+
+    Parameters
+    ----------
+    data_in : Density
+        Data to be condensed into 1 ms time steps
+
+    Returns
+    -------
+    data_out : Density
+        Data in 1 ms time steps.
+    """
+    # look up old scripts for this. should be pretty similar. maybe try to add dynamic adjustments, but not sure how.
 
 
 # store the data as hdf5?
