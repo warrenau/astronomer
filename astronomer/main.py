649825e3c9856422c618b183e77c53d77740bb18
warrenau <warrenau@oregonstate.edu>
2022-03-15 20:37:59 -0700

    added examples to README
    added python highlighting to code blocks in README
    cleaned up main.py file

--- astronomer/main.py
+++ astronomer/main.py
@@ -4,11 +4,10 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from pathlib import Path
 import density as dens
 
 
-# read in data
+# inputs
 filepath = 'astronomer/Data/'
 filename = 'HENRI_250psi_HeatGen_TS_density'
 f = filepath+filename+'.csv'
@@ -18,26 +17,10 @@ positions = ('TS00', 'TS01', 'TS02', 'TS03', 'TS04', 'TS05')
 time_step = np.array([0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009])
 
 
-##########
-# this part of the script will determine what other scripts need to be called
-# this will depend on what outputs you are trying to process
-##########
 
-
-
-##########
-# this part of the script will run the other scripts needed to process the data
-# this includes scripts for plotting, storing data, and even generating reports
-##########
-
-
-#
+# functions
 density_data = dens.data_to_density(f,positions)
-
 dens.plot_data(density_data, filename)
-
 atomdensity_data = dens.density_to_atomdensity(density_data)
-
-density_data_step = dens.get_time_step_data(atomdensity_data, time_step)
-
-dens.writeDensity(density_data_step, filepath+filename+'_step.csv')
+atomdensity_data_step = dens.get_time_step_data(atomdensity_data, time_step)
+dens.writeDensity(atomdensity_data_step, filepath+filename+'_step.csv')
